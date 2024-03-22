8311bbbb548b892738c733674ce2ed22aaa6911f
Manvendra Singh <MANVENDRAS2608@GMAIL.COM>
2024-03-22 15:08:10 +0530

    Data Ingestion Notebook completed

--- template.py
+++ template.py
@@ -21,7 +21,7 @@ list_of_files = [
     f"src/{project_name}/constants/__init__.py",
     "config/config.yaml",
     "params.yaml",
-    "schema.yaml",
+    "schemas.yaml",
     "main.py",
     "app.py",
     "requirements.txt",
